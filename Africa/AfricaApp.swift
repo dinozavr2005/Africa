9de428ab2b51d54c5bbeea0b5e569b751a59adc2
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2021-11-24 16:31:33 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Владимир on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
